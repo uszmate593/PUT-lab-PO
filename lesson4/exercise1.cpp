aa61d330cf40e24cf5f528e423a6e6601457530f
Mateusz Barski <95314247+uszmate593@users.noreply.github.com>
2022-11-17 16:22:27 +0100

    Add files via upload

---
+++ lesson4/exercise1.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+
+using namespace std;
+
+class Room
+{
+    public:
+    virtual string checkWhatIsHappening() = 0;
+    virtual float checkSize() = 0;
+    virtual int checkHowManyPeopleCanGetInside() = 0;
+};
+
+class Classrom : public Room
+{
+    private:
+    float width, lenght;
+    int capacity;
+    string currentLesson;
+    public:
+    Classrom(float width, float lenght, int capacity, string currentLesson)
+    {
+        this->width = width;
+        this->lenght = lenght;
+        this->capacity = capacity;
+        this->currentLesson = currentLesson;
+    }
+    
+    string checkWhatIsHappening()
+    {
+        return currentLesson;
+    }
+
+    float checkSize()
+    {
+        return width * lenght;
+    }
+
+    int checkHowManyPeopleCanGetInside()
+    {
+        return capacity;
+    }
+
+    Classrom beginNextLesson(string lessonName)
+    {
+        return new Classrom(width, length, capacity, lessonName);
+    }
+};
+
+int main()
+{
+    Classrom first(11.2, 10.0, 25, "English");
+    Classrom second = first.beginNextLesson("Math");
+    return 0;
+}
