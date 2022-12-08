278161eb131d5852634832fbb25b457324975182
uszmate593 <95314247+uszmate593@users.noreply.github.com>
2022-12-08 16:37:45 +0100

    lesson7

--- lesson7/Day01/elf.cpp
+++ lesson7/Day01/elf.cpp
@@ -9,5 +9,8 @@ Elf::Elf(vector<int> callories)
 
 int Elf::calloriesCaried()
 {
-    
+    int sum = 0;
+    for(int i = 0;i < callories.size();i++)
+        sum += callories[i];
+    return sum;
 }
--- lesson7/Day01/main.cpp
+++ lesson7/Day01/main.cpp
@@ -1,8 +1,37 @@
 #include"elf.hpp"
+#include<string>
+#include<iostream>
+#include<fstream>
 
 using namespace std;
 
 int main()
 {
-    
+    vector<Elf> elves;
+    vector<int> callories;
+    ifstream input;
+    input.open("Day01.txt");
+    string inputString;
+    int maxCallories = 0;
+    int carriedCallories;
+    while(getline(input, inputString))
+    {
+        if(inputString == "\n")
+        {
+            elves.push_back(Elf(callories));
+            while(callories.size() != 0)
+                elves.pop_back();
+        }
+        else
+            callories.push_back(stoi(inputString));
+    }
+    input.close();
+    for(int i = 0;i < elves.size();i++)
+    {
+        carriedCallories = elves[i].calloriesCaried();
+        if(carriedCallories > maxCallories)
+            maxCallories = carriedCallories;
+    }
+    cout << maxCallories << endl;
+    return 0;
 }
